6d754779ca3330766bd3baf3d5a80ab723a04c46
sneyker_408 <sneyker408@hotmail.com>
2024-08-15 21:20:41 -0600

    añadiendo Clausula First y FirstOrDefault

---
+++ Guia de proyectos/Semana 1/Clausula First y FirstOrDefault/Metodo_first/Casa.cs
@@ -0,0 +1,11 @@
+﻿public class Casa
+{
+    public int Id { get; set; }
+    public string Direccion { get; set; }
+    public string Ciudad { get; set; }
+    public int numeroHabitaciones { get; set; }
+    public string dameDatosCasa()
+    {
+        return $"Direcion es {Direccion} en la ciudad de {Ciudad} con numero de habitaciones {numeroHabitaciones}";
+    }
+}
---
+++ Guia de proyectos/Semana 1/Clausula First y FirstOrDefault/Metodo_first/Habitante.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroduccionLinq
+{
+    public class Habitante
+    {
+        public int IdHabitante { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int IdCasa { get; set; }
+
+        public string datosHabitante()
+        {
+
+            return $"Soy {Nombre} con edad de {Edad} años vividos en {IdCasa}";
+        }
+    }
+}
