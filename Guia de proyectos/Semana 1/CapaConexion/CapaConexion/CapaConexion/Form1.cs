5d98f89422e04591d3e7d00dddfa880b9486fb03
sneyker_408 <sneyker408@hotmail.com>
2024-08-20 17:26:01 -0600

    Guía VIII: Recomendaciones para el uso de Índices en consultas SQL

--- Guia de proyectos/Semana 1/CapaConexion/CapaConexion/CapaConexion/Form1.cs
+++ Guia de proyectos/Semana 1/CapaConexion/CapaConexion/CapaConexion/Form1.cs
@@ -21,23 +21,43 @@ namespace CapaConexion
         private void btnCargar_Click(object sender, EventArgs e)
         {
             SqlConnection conexion =
-        new SqlConnection
-        ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True");
+            new SqlConnection
+            ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;");
             MessageBox.Show("Conexion creada");
             conexion.Open();
-            string selectFrom = "SELECT * FROM [dbo].[Customers]";
+
+
+
+            //------------------------
+
+            String selectFrom = "";
+            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
+            selectFrom = selectFrom + "      ,[Address] " + "\n";
+            selectFrom = selectFrom + "      ,[City] " + "\n";
+            selectFrom = selectFrom + "      ,[Region] " + "\n";
+            selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
+            selectFrom = selectFrom + "      ,[Country] " + "\n";
+            selectFrom = selectFrom + "      ,[Phone] " + "\n";
+            selectFrom = selectFrom + "      ,[Fax] " + "\n";
+            selectFrom = selectFrom + "  FROM [dbo].[Customers]";
+
+            //-----------------------
+
+
+
 
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
             while (reader.Read())
             {
-                var customerId = reader[0];
+                var customerId = reader["CompanyName"];
             }
 
             MessageBox.Show("Conexion cerrada");
-            conexion.Close(); ;
-
+            conexion.Close();
         }
     }
 }
