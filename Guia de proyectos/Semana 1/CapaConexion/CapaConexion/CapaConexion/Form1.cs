e4c25da6b555457e383e2764410bfa62d61b9c77
sneyker_408 <sneyker408@hotmail.com>
2024-08-20 18:50:16 -0600

    Guía X  Conectar la base de datos a la interfaz grafica

--- Guia de proyectos/Semana 1/CapaConexion/CapaConexion/CapaConexion/Form1.cs
+++ Guia de proyectos/Semana 1/CapaConexion/CapaConexion/CapaConexion/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Documents;
 
 namespace CapaConexion
 {
@@ -21,17 +22,16 @@ namespace CapaConexion
         private void btnCargar_Click(object sender, EventArgs e)
         {
             SqlConnection conexion =
-            new SqlConnection
-            ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;");
+                new SqlConnection
+                ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;");
             MessageBox.Show("Conexion creada");
             conexion.Open();
-
-
 
             //------------------------
 
             String selectFrom = "";
-            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "SELECT " + "\n";
+            selectFrom = selectFrom + "      [CompanyName] " + "\n";
             selectFrom = selectFrom + "      ,[ContactName] " + "\n";
             selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
             selectFrom = selectFrom + "      ,[Address] " + "\n";
@@ -45,16 +45,29 @@ namespace CapaConexion
 
             //-----------------------
 
-
-
-
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
+            List<Customers> Customers = new List<Customers>();
+
             while (reader.Read())
             {
-                var customerId = reader["CompanyName"];
+
+                Customers customers = new Customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? 0 : (int)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                Customers.Add(customers);
             }
+        }
 
             MessageBox.Show("Conexion cerrada");
             conexion.Close();
---
+++ Guia de proyectos/Semana 1/CapaConexion/CapaConexion/CapaConexion/Modelos/customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaConexion.Modelos
+{
+    internal class customers
+    {
+        public String CompanyName { get; set; }
+        public String ContactName { get; set; }
+        public String ContactTitle { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Region { get; set; }
+        public String PostalCode { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+        public String Fax { get; set; }
+    }
+}
