67c8fbce9a36f77069631137bc3656e631a752ed
sneyker_408 <sneyker408@hotmail.com>
2024-08-27 16:05:01 -0600

    Guía IX : generando un modelo y extrayendo la información.

--- Guia de proyectos/Semana 1/DataSourceDemo/CapaConexion/Form1.cs
+++ Guia de proyectos/Semana 1/DataSourceDemo/CapaConexion/Form1.cs
@@ -25,14 +25,35 @@ namespace CapaConexion
                 ("Data Source=DESKTOP-Q5074QV;Initial Catalog=Northwind;Integrated Security=True;");
             MessageBox.Show("Conexion creada");
             conexion.Open();
-            string selectFrom = "SELECT * FROM [dbo].[Customers]";
+
+
+
+            //------------------------
+
+            String selectFrom = "";
+            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
+            selectFrom = selectFrom + "      ,[Address] " + "\n";
+            selectFrom = selectFrom + "      ,[City] " + "\n";
+            selectFrom = selectFrom + "      ,[Region] " + "\n";
+            selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
+            selectFrom = selectFrom + "      ,[Country] " + "\n";
+            selectFrom = selectFrom + "      ,[Phone] " + "\n";
+            selectFrom = selectFrom + "      ,[Fax] " + "\n";
+            selectFrom = selectFrom + "  FROM [dbo].[Customers]";
+
+            //-----------------------
+
+
+
 
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
             while (reader.Read())
             {
-                var customerId = reader[0];
+                var customerId = reader["CompanyName"];
             }
 
             MessageBox.Show("Conexion cerrada");
