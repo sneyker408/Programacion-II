189b2e855ead137ebb7b0842e93ad2cbf341263a
sneyker_408 <sneyker408@hotmail.com>
2024-09-04 00:14:35 -0600

    Guía XIV: Cerrando la conexión de manera automática

--- Guia de proyectos/Semana 1/CapaConexion/DatosLayer/CustomerRepository.cs
+++ Guia de proyectos/Semana 1/CapaConexion/DatosLayer/CustomerRepository.cs
@@ -11,45 +11,47 @@ namespace DatosLayer
     {
         public List<Customers> ObtenerTodos()
         {
-
-            var conexion = DataBase.GetSqlConnection();
-            String selectFrom = "";
-            selectFrom = selectFrom + "SELECT " + "\n";
-            selectFrom = selectFrom + "      [CompanyName] " + "\n";
-            selectFrom = selectFrom + "      ,[ContactName] " + "\n";
-            selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
-            selectFrom = selectFrom + "      ,[Address] " + "\n";
-            selectFrom = selectFrom + "      ,[City] " + "\n";
-            selectFrom = selectFrom + "      ,[Region] " + "\n";
-            selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
-            selectFrom = selectFrom + "      ,[Country] " + "\n";
-            selectFrom = selectFrom + "      ,[Phone] " + "\n";
-            selectFrom = selectFrom + "      ,[Fax] " + "\n";
-            selectFrom = selectFrom + "  FROM [dbo].[Customers]";
-
-            SqlCommand comando = new SqlCommand(selectFrom, conexion);
-            SqlDataReader reader = comando.ExecuteReader();
-            // La lista que sera llenada con objetos Customers
-            List<Customers> Customers = new List<Customers>();
-
-            while (reader.Read())
+            using (var conexion = DataBase.GetSqlConnection())
             {
-                Customers customers = new Customers(); // objeto que llena la lista
-                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
-                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
-                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
-                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
-                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
-                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
-                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (String)reader["PostalCode"];
-                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
-                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
-                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+                String selectFrom = "";
+                selectFrom = selectFrom + "SELECT " + "\n";
+                selectFrom = selectFrom + "      [CompanyName] " + "\n";
+                selectFrom = selectFrom + "      ,[ContactName] " + "\n";
+                selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
+                selectFrom = selectFrom + "      ,[Address] " + "\n";
+                selectFrom = selectFrom + "      ,[City] " + "\n";
+                selectFrom = selectFrom + "      ,[Region] " + "\n";
+                selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
+                selectFrom = selectFrom + "      ,[Country] " + "\n";
+                selectFrom = selectFrom + "      ,[Phone] " + "\n";
+                selectFrom = selectFrom + "      ,[Fax] " + "\n";
+                selectFrom = selectFrom + "  FROM [dbo].[Customers]";
 
-                Customers.Add(customers);
+                using (SqlCommand comando = new SqlCommand(selectFrom, conexion))
+                {
+                    SqlDataReader reader = comando.ExecuteReader();
+                    List<Customers> Customers = new List<Customers>();
+
+                    while (reader.Read())
+                    {
+                        Customers customers = new Customers();
+                        customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                        customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                        customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                        customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                        customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                        customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                        customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (String)reader["PostalCode"];
+                        customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                        customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                        customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                        Customers.Add(customers);
+                    }
+                    return Customers;
+                }
             }
-            conexion.Close();
-            return Customers;
+
         }
     }
 }
