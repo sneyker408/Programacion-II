eaf603a9d899c9b373ab31cbcef3031149494f77
sneyker_408 <sneyker408@hotmail.com>
2024-09-03 23:56:12 -0600

    Guía XIV: Cerrando la conexión de manera automática.

--- Guia de proyectos/Semana 1/CapaConexion/ConexionEjemplo/Customers.cs
+++ Guia de proyectos/Semana 1/CapaConexion/ConexionEjemplo/Customers.cs
@@ -3,5 +3,14 @@
     public class Customers
     {
         public string CompanyName { get; internal set; }
+        public string ContactName { get; internal set; }
+        public string ContactTitle { get; internal set; }
+        public string Address { get; internal set; }
+        public string City { get; internal set; }
+        public string Region { get; internal set; }
+        public string PostalCode { get; internal set; }
+        public string Phone { get; internal set; }
+        public string Country { get; internal set; }
+        public string Fax { get; internal set; }
     }
 }
