223e415623840a6fc077229b4ef11f104e6bc1c2
sneyker_408 <sneyker408@hotmail.com>
2024-09-04 00:35:59 -0600

    Guía XIV: Cerrando la conexión de manera automática mejorado

--- Guia de proyectos/Semana 1/CapaConexion/ConexionEjemplo/Form1.cs
+++ Guia de proyectos/Semana 1/CapaConexion/ConexionEjemplo/Form1.cs
@@ -13,51 +13,36 @@ using System.Windows.Forms;
 namespace ConexionEjemplo
 {
     // Codigo Actual Form1
-    public class CustomerRepository
+    public partial class Form1 : Form
     {
-        public List<Customers> ObtenerTodos()
+        CustomerRepository customerRepository = new CustomerRepository();
+
+        public Form1()
         {
-            using (var conexion = DataBase.GetSqlConnection())
-            {
-                String selectFrom = "";
-                selectFrom = selectFrom + "SELECT " + "\n";
-                selectFrom = selectFrom + "      [CompanyName] " + "\n";
-                selectFrom = selectFrom + "      ,[ContactName] " + "\n";
-                selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
-                selectFrom = selectFrom + "      ,[Address] " + "\n";
-                selectFrom = selectFrom + "      ,[City] " + "\n";
-                selectFrom = selectFrom + "      ,[Region] " + "\n";
-                selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
-                selectFrom = selectFrom + "      ,[Country] " + "\n";
-                selectFrom = selectFrom + "      ,[Phone] " + "\n";
-                selectFrom = selectFrom + "      ,[Fax] " + "\n";
-                selectFrom = selectFrom + "  FROM [dbo].[Customers]";
+            InitializeComponent();
+        }
 
-                using (SqlCommand comando = new SqlCommand(selectFrom, conexion))
-                {
-                    SqlDataReader reader = comando.ExecuteReader();
-                    List<Customers> Customers = new List<Customers>();
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            var Customers = customerRepository.ObtenerTodos();
+            dataGrid.DataSource = Customers;
+        }
 
-                    while (reader.Read())
-                    {
-                        Customers customers = new Customers();
-                        customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
-                        customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
-                        customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
-                        customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
-                        customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
-                        customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
-                        customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (String)reader["PostalCode"];
-                        customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
-                        customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
-                        customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // var filtro = Customers.FindAll( X => X.CompanyName.StartsWith(tbFiltro.Text));
+            // dataGrid.DataSource = filtro;
+        }
 
-                        Customers.Add(customers);
-                    }
-                    return Customers;
-                }
-            }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+            /*  DatosLayer.DataBase.ApplicationName = "Programacion 2 ejemplo";
+             DatosLayer.DataBase.ConnectionTimeout = 30;
+
+             string cadenaConexion = DatosLayer.DataBase.ConnectionString;
+               var conxion = DatosLayer.DataBase.GetSqlConnection();
+            */
         }
     }
 }
