48b9c887993557b3305f53f8925e8441ef45ea38
sneyker_408 <sneyker408@hotmail.com>
2024-10-07 17:18:11 -0600

    Guía I: introducción a Dapper ORM (1)

---
+++ Guia de proyectos/guias Introdccion a Dapper/AccesoDatos/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class DataBase
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["NWConnectionString"].ConnectionString;
+            }
+        }
+        public static SqlConnection GetSqlConnection()
+        {
+
+            SqlConnection conexion = new SqlConnection(ConnectionString);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
