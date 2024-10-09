0c78627f3e9d3970d322820c2bbf8bc12ec1a4e5
sneyker_408 <sneyker408@hotmail.com>
2024-10-08 20:14:44 -0600

    Guía 4: Controladores (1)

---
+++ Guia de proyectos/Backend ASP.NET Core API/Backend/Backend/Controllers/OperationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        [HttpGet]
+
+        public decimal add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
