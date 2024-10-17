bdd50c42d4e73db4040b28e82c1960d843726324
sneyker_408 <sneyker408@hotmail.com>
2024-10-16 20:19:05 -0600

    Guía 8: Respuestas de dato especifico. (1)

---
+++ Guia de proyectos/Backend ASP.NET Core API/Backend/Backend/Controllers/PersonaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Backend.Controllers.Repository;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<PersonaDatos> GetPersonaDatos() => Repository.persona;
+    }
+}
---
+++ Guia de proyectos/Backend ASP.NET Core API/Backend/Backend/Controllers/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace Backend.Controllers
+{
+    public class Repository
+    {
+        public static List<PersonaDatos> persona = new List<PersonaDatos> {
+        new PersonaDatos()
+        {
+            id = 1,
+            age = new DateTime(1994,08,03),
+            name = "Erick Diaz"
+        },
+        new PersonaDatos()
+        {
+            id = 2,
+            age = new DateTime(1994, 09, 03),
+            name = "Elver Galarga"
+        },
+        new PersonaDatos()
+        {
+            id = 3,
+            age = new DateTime(1994, 11, 03),
+            name = "Seria"
+        }
+
+        };
+    }
+    public class PersonaDatos
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime age { get; set; }
+        public string email { get; set; }
+
+    }
+}
